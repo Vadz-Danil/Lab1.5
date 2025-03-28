783345eb966c2f9a7266c40477f6e9c29a77943c
Vadz-Danil <vadz.danil@gmail.com>
2025-03-28 11:06:05 +0200

    Update Main2.java

--- Main2.java
+++ Main2.java
@@ -4,6 +4,9 @@
      bst1.insert(new Student("Orlova", "Ekaterina",2,false));
      bst1.insert(new Student("Zubko", "Taras",3,true));
      bst1.insert(new Student("Petrus", "Viktor",4,false));
+     bst1.insert(new Student("Tarasiuk", "Vasyl",5,false));
+     System.out.println("Усі студенти:");
+     bst1.print();
      String searchingName = "Evgeny";
      bst1.find(searchingName);
 }
